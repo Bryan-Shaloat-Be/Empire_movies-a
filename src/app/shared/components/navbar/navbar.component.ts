d13acb6d15c7ef7c007c31f860160ea69bbe6e29
Bryan Shaloat Be <64609024+Bryan-Shaloat-Be@users.noreply.github.com>
2024-07-06 19:27:13 -0600

    routing, design navbar and new lateral-menu

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { ArrayType } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
-  
+export class NavbarComponent implements OnInit{
+  menu: boolean = false
+  link_menu: {
+    defaultOptions: Array<any>, accesslink: Array<any>
+  } = {defaultOptions:[], accesslink:[] }
+
+  onclickmenu(){
+    this.menu = !this.menu;
+  }
+
+  ngOnInit(): void {
+    this.link_menu.defaultOptions = [
+      {
+        name: "Inicio",
+        router:['/']
+      },
+      {
+        name: "Categorias",
+        router:['/']
+      },
+    ] 
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SectionMoviesComponent } from './components/section-movies/section-movies.component';
 import { CardMovieComponent } from './components/card-movie/card-movie.component';
+import { RecommendationMoviesComponent } from './components/recommendation-movies/recommendation-movies.component';
+import { HistoryViewComponent } from './components/history-view/history-view.component';
 
 
 
@@ -12,7 +14,9 @@ import { CardMovieComponent } from './components/card-movie/card-movie.component
     NavbarComponent,
     FooterComponent,
     SectionMoviesComponent,
-    CardMovieComponent
+    CardMovieComponent,
+    RecommendationMoviesComponent,
+    HistoryViewComponent
   ],
   imports: [
     CommonModule
@@ -20,7 +24,9 @@ import { CardMovieComponent } from './components/card-movie/card-movie.component
   exports:[
     NavbarComponent,
     SectionMoviesComponent,
-    CardMovieComponent
+    CardMovieComponent,
+    RecommendationMoviesComponent,
+    HistoryViewComponent
   ]
 })
 export class SharedModule { }
