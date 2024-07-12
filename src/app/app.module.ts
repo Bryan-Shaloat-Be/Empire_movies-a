a61ec39213d177cef468b26f81e615162534af2e
Bryan Shaloat Be <64609024+Bryan-Shaloat-Be@users.noreply.github.com>
2024-07-12 14:47:11 -0600

    little some bugs. Add proyect imgs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,10 @@
 import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from '@modules/home/page/homepage/homepage.component';
 import { CategoryModule } from '@modules/category/category.module';
 import { SharedModule } from '@shared/shared.module';
-import { HomeModule } from '@modules/home/home.module';
-import { AuthModule } from '@modules/auth/auth.module';
-import { MoviesModule } from '@modules/movies/movies.module';
-import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
@@ -17,14 +12,7 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    CategoryModule,
-    SharedModule,
-    HomeModule,
-    AuthModule,
-    MoviesModule,
-    RouterModule
-   
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
