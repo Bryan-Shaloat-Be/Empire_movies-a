b0adfed9e903813cc1d8d2c4020961c40cedf9c2
Bryan Shaloat Be <64609024+Bryan-Shaloat-Be@users.noreply.github.com>
2024-07-20 12:23:54 -0600

    changes on auth component

--- src/app/modules/auth/pages/authpage/authpage.component.ts
+++ src/app/modules/auth/pages/authpage/authpage.component.ts
@@ -14,6 +14,29 @@ export class AuthpageComponent implements OnInit{
   formlogingroup : FormGroup = new FormGroup({});
   change_view: boolean = false;
 
+  items_select: Array<any> = [
+    {value: 'Accion', viewValue: 'Accion'},
+    {value: 'Animacion', viewValue: 'Animacion'},
+    {value: 'Aventura', viewValue: 'Aventura'},
+    {value: 'Disparos', viewValue: 'Disparos'},
+    {value: 'Catastrofe', viewValue: 'Catastrofe'},
+    {value: 'Ciencia Ficcion', viewValue: 'Ciencia Ficcion'},
+    {value: 'Comedia', viewValue: 'Comedia'},
+    {value: 'Crimen', viewValue: 'Crimen'},
+    {value: 'Deportes', viewValue: 'Deportes'},
+    {value: 'Documentales', viewValue: 'Documentales'},  // Lista de categorias para elegir en el apartado de preferencias 
+    {value: 'Drama', viewValue: 'Drama'},
+    {value: 'Epicas', viewValue: 'Epicas'},
+    {value: 'Fantasia', viewValue: 'Fantasia'},
+    {value: 'Futurista', viewValue: 'Futurista'},
+    {value: 'Historico', viewValue: 'Historico'},
+    {value: 'Musical', viewValue: 'Musical'},
+    {value: 'Religion', viewValue: 'Religion'},
+    {value: 'Romance', viewValue: 'Romance'},
+    {value: 'Suspenso', viewValue: 'Suspenso'},
+    {value: 'Terror', viewValue: 'Terror'},
+  ]
+
   constructor(public router: Router){}
 
 
