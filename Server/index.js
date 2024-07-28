2629bac9c78a9782617fbbdb56933ab2294b6ab7
Bryan Shaloat Be <64609024+Bryan-Shaloat-Be@users.noreply.github.com>
2024-07-27 23:29:16 -0600

    back-end of Category
    - we need to add conection with client

--- Server/index.js
+++ Server/index.js
@@ -3,8 +3,9 @@ const {sql, poolPromise} = require('./app/models/DB'); //importacion de la confi
 const bycript = require('bcrypt');
 const cors = require('cors'); //Para conseguir el uso del api con angular
 const userRoutes = require('./app/routes/users_routes');
-const mediaRoutes = require('./app/routes/MediaRoutes')
-const favoritesRoutes = require('./app/routes/favoritesRoute')
+const mediaRoutes = require('./app/routes/MediaRoutes');
+const favoritesRoutes = require('./app/routes/favoritesRoute');
+const categoriasMedia = require('./app/routes/CategoryRoute');
 const path = require('path');
 
 
@@ -14,6 +15,7 @@ app.use(cors());
 app.use('/register', userRoutes); //rutas de registro y login de usuarios
 app.use('/movies', mediaRoutes); // Rutas para obtener peliculas y series 
 app.use('/favorites',favoritesRoutes); // Rutas para obtener favoritos del usuario
+app.use('/category',categoriasMedia); // Rutas para obtener peliculas y series con filtro de categoria 
 
 app.use('/img', express.static(path.join(__dirname, 'app/img'))); // rutas estaticas para acceder a las imagenes de las peliculas
                                                                     // ----- series 
