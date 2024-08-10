48bedf0db426019c47b037bb32a98b147044aa5a
Bryan Shaloat Be <64609024+Bryan-Shaloat-Be@users.noreply.github.com>
2024-08-09 23:03:40 -0600

    x

--- Server_Asp.net/ServerAsp/obj/Debug/net8.0/ServerAsp.AssemblyInfo.cs
+++ Server_Asp.net/ServerAsp/obj/Debug/net8.0/ServerAsp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerAsp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edfbb953de937e0dda9fa8e908a211f93f316d11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+060c18df17fe5936e4bb143b2614300aee9a8ce3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerAsp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerAsp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
