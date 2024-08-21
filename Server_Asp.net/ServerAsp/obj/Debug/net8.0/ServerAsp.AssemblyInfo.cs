1535e6dd9a6bfb97482ceab62ff1fe652bf475cf
Bryan Shaloat Be <64609024+Bryan-Shaloat-Be@users.noreply.github.com>
2024-08-21 16:31:08 -0600

    credentials

--- Server_Asp.net/ServerAsp/obj/Debug/net8.0/ServerAsp.AssemblyInfo.cs
+++ Server_Asp.net/ServerAsp/obj/Debug/net8.0/ServerAsp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerAsp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+875404c0d989b9d2178c75b693ae190dbde7ae8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdedf32905a7f446264b317976de859a1a74ea16")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerAsp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerAsp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
